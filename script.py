92ee623fb8d686a85bb315785ad76f5fd70cceb6
Qweku <opnarh@gmail.com>
2020-05-12 20:52:53 +0200

    csv files and updates

--- script.py
+++ script.py
@@ -4,14 +4,40 @@ import sorts
 bookshelf = utils.load_books('books_small.csv')
 
 for book in bookshelf:
-  print(book['title_lower'])
+    print(book['title_lower'])
+
 
 def by_title_ascending(book_a, book_b):
-  if book_a['title_lower'] > book_b['title_lower']:
-    return True
-  return False
+    if book_a['title_lower'] > book_b['title_lower']:
+        return True
+    return False
+
 
 sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
 
 for book in sort_1:
-  print(book['title'])
+    print(book['title'])
+
+
+# function to sort books by author
+def by_author_ascending(book_a, book_b):
+    if book_a['author_lower'] > book_b['author_lower']:
+        return True
+    return False
+
+bookshelf_v1 = bookshelf[:]
+
+sort_2 = sorts.bubble_sort(bookshelf_v1, by_author_ascending)
+
+for book in sort_2:
+    print(book['title'])
+
+bookshelf_v2 = bookshelf[:]
+
+sorts.quicksort(bookshelf_v2, 0, len(bookshelf_v2) - 1, by_author_ascending)
+
+def by_total_length(book_a, book_b):
+    if book_a['author_lower'] > book_b['author_lower']:
+        return True
+    return False
+
--- sorts.py
+++ sorts.py
@@ -13,7 +13,7 @@ def bubble_sort(arr,comparison_function):
   print("Bubble sort: There were {0} swaps".format(swaps))
   return arr
 
-def quicksort(list, start, end):
+def quicksort(list, start, end, comparison_function):
   if start >= end:
     return
   pivot_idx = random.randrange(start, end + 1)
@@ -21,9 +21,9 @@ def quicksort(list, start, end):
   list[end], list[pivot_idx] = list[pivot_idx], list[end]
   less_than_pointer = start
   for i in range(start, end):
-    if pivot_element > list[i]:
+    if comparison_function(pivot_element, list[i]):
       list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
       less_than_pointer += 1
   list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
-  quicksort(list, start, less_than_pointer - 1)
-  quicksort(list, less_than_pointer + 1, end)
+  quicksort(list, start, less_than_pointer - 1, comparison_function)
+  quicksort(list, less_than_pointer + 1, end, comparison_function)
